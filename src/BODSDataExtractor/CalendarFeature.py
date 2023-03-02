0a52b21a740836d00a4fc28e5c9ccdacd6fe460a
Isri Rai <isri.rai@kpmg.co.uk>
2023-03-02 12:27:12 +0000

    Edited extractor file (services_on_bods_or_otc_by_area_just_otc & services_on_bods_or_otc_by_area)

--- src/BODSDataExtractor/CalendarFeature.py
+++ src/BODSDataExtractor/CalendarFeature.py
@@ -4,6 +4,7 @@ Created on Wed Jan  4 12:37:43 2023
 
 @author: -loper-irai
 """
+# version: 1.0
 
 from extractor import TimetableExtractor
 import pandas as pd
@@ -21,52 +22,52 @@ my_bus_data_object = TimetableExtractor(api_key=api
                                         , stop_level=False
                                         )
 
+# Returns a copy of the service line level data suitable for analysis and filters columns
 analytical_timetable_data_without_duplicates = my_bus_data_object.analytical_timetable_data_analysis()
-
-# get relevant fields
 calendar_df = analytical_timetable_data_without_duplicates[['DatasetID', 'OperatorName', 'FileName', 'TradingName',
                                                             'ServiceCode', 'LineName', 'OperatingPeriodStartDate',
                                                             'OperatingPeriodEndDate', 'RevisionNumber',
                                                             'OperatingDays']]
 
 calendar_df.to_csv('C:\\Users\\irai\\OneDrive - KPMG\\BODS\\BDE '
-                   'Package\\src\\BODSDataExtractor\\calendar_df_BEFORE_24.02.2023.csv')
+                   'Package\\src\\BODSDataExtractor\\calendar_df_BEFORE_02.03.2023_new.csv')
 
-# get dates 42 days from current date
+# Select dates 42 days from today's date. These will be added to the calendar view dataframe (calendar_df)
 current_date = pd.to_datetime('today').normalize()
 expected_final_published_date = current_date + pd.Timedelta(days=42)
 
-# add the dates to the calendar_df
+# Add the dates to the calendar_df and assign all values as empty
 delta = dt.timedelta(days=1)
 while current_date <= expected_final_published_date:
     calendar_df[current_date] = None
     print(current_date, end='\n')
     current_date += delta
 
-# convert data types for analysis
+# Convert data types for analysis
 calendar_df['OperatingPeriodStartDate'] = pd.to_datetime(calendar_df['OperatingPeriodStartDate'])
 calendar_df['OperatingPeriodEndDate'] = pd.to_datetime(calendar_df['OperatingPeriodEndDate'])
 calendar_df.reset_index(inplace=True)
 
-# put assign value 'true' where calendar date is between or equal to the operating period start and end date
-for j in range(11, 54):
+# Iterate through each calendar date column. For each date iterate through each row and assign value 'true'
+# where calendar date is between or equal to the operating period start and end date
+for calendar_date in range(11, 54):
     for row in calendar_df.itertuples():
-        print(calendar_df.columns[j])
+        print(calendar_df.columns[calendar_date])
         print(getattr(row, 'OperatingPeriodStartDate'))
         if (getattr(row, 'OperatingPeriodEndDate') is pd.NaT) or (getattr(row, 'OperatingPeriodEndDate') is None):
-            print(calendar_df.columns[j])
+            print(calendar_df.columns[calendar_date])
             print(getattr(row, 'OperatingPeriodStartDate'))
-            if calendar_df.columns[j] >= getattr(row, 'OperatingPeriodStartDate'):
-                calendar_df[calendar_df.columns[j]][getattr(row, 'Index')] = "True"
-        elif (calendar_df.columns[j] >= getattr(row, 'OperatingPeriodStartDate')) and \
-                (calendar_df.columns[j] <= getattr(row, 'OperatingPeriodEndDate')):
-            calendar_df[calendar_df.columns[j]][getattr(row, 'Index')] = "True"
+            if calendar_df.columns[calendar_date] >= getattr(row, 'OperatingPeriodStartDate'):
+                calendar_df[calendar_df.columns[calendar_date]][getattr(row, 'Index')] = "True"
+        elif (calendar_df.columns[calendar_date] >= getattr(row, 'OperatingPeriodStartDate')) and \
+                (calendar_df.columns[calendar_date] <= getattr(row, 'OperatingPeriodEndDate')):
+            calendar_df[calendar_df.columns[calendar_date]][getattr(row, 'Index')] = "True"
 
 # for testing
 calendar_df.to_csv('C:\\Users\\irai\\OneDrive - KPMG\\BODS\\BDE '
-                   'Package\\src\\BODSDataExtractor\\calendar_df_sorted_24.02.2023.csv')
+                   'Package\\src\\BODSDataExtractor\\calendar_df_sorted_02.03.2023_new.csv')
 
-# collect all dates from column names 11 onwards
+# Gets a list of all dates from calendar_df
 columns = []
 for date in calendar_df.columns:
     columns.append(date)
@@ -76,9 +77,8 @@ operator_df = calendar_df.sort_values(by=['ServiceCode', 'LineName', 'RevisionNu
                                       , ascending=True)
 
 operator_df = operator_df.reset_index()
-# Group operator_df by ServiceCode, LineName, and OperatingDays
 
-# converts dates with dashes e.g. Monday-Friday to Monday, Tuesday, Wednesday, Thursday, Friday to enable comparison
+# Converts dates with dashes e.g. Monday-Friday to Monday, Tuesday, Wednesday, Thursday, Friday to enable comparison
 for operatingDay in operator_df['OperatingDays']:
     if "-" in operatingDay:
         first_date = operatingDay[0:operatingDay.find('-')]
@@ -90,48 +90,65 @@ for operatingDay in operator_df['OperatingDays']:
         new_output = ",".join(new_output)
         operator_df.replace(operatingDay, new_output, inplace=True)
 
-# create new column for grouping
+# Create new column to group rows where the operating period for a service-line combo is a subset of an equal/higher
+# revision number file
 operator_df['DaysGroup'] = ""
 operator_df.sort_values(by=['ServiceCode', 'LineName', 'RevisionNumber'])
 groups = operator_df.groupby(['ServiceCode', 'LineName'])
 
 # Iterate through each row in the group (current_index) and for each row iterate through the next rows (next_index).
 # Check if all the operating days for the current_index are in the next_index's operating days. If they are a subset
-# assign both rows the same counter
+# assign both rows the same counter in the 'DaysGroup' column
 counter = 0
 for name, group in groups:
-    pd.set_option('display.max_columns', None)
     for current_index in group.index:
-        print(type(current_index))
-        if (current_index == 5):
-            print('hhh')
         counter += 1
-        if current_index + 1 in group.index:
-            for next_index in range(current_index + 1, current_index + len(group)):
+        # check if the revision numbers are all the same. If they are the same then 'next_index' goes through all the
+        # rows. If they are not the same then only go through the rows with a higher revision number.
+        if group.RevisionNumber.nunique() == 1:
+            print(group.RevisionNumber)
+            print(group.iloc[0].name)
+            calculated_next_index = group.iloc[0].name
+        else:
+            calculated_next_index = current_index+1
+        if calculated_next_index in group.index:
+            print(calculated_next_index)
+            last_index = group.iloc[-1].name
+            for next_index in range(calculated_next_index, last_index+1):
                 if next_index in group.index:
                     operating_days_in_current_index = group.loc[current_index, 'OperatingDays']
                     operating_days_next_index = group.loc[next_index, 'OperatingDays']
                     operating_days_in_current_index = operating_days_in_current_index.split(",")
                     operating_days_next_index = operating_days_next_index.split(",")
                     if all([item in operating_days_next_index for item in operating_days_in_current_index]):
-                        if (operator_df.loc[next_index, 'DaysGroup'] == "") & (operator_df.loc[current_index, 'DaysGroup'] == ""):
+                        if (operator_df.loc[next_index, 'DaysGroup'] == "") & (
+                                operator_df.loc[current_index, 'DaysGroup'] == ""):
+                            print(current_index)
+                            print(next_index)
                             operator_df.at[current_index, 'DaysGroup'] = counter
                             operator_df.at[next_index, 'DaysGroup'] = counter
-                        elif (operator_df.loc[next_index, 'DaysGroup'] == "") & (operator_df.loc[current_index, 'DaysGroup'] != ""):
+                            group.at[current_index, 'DaysGroup'] = counter
+                            group.at[next_index, 'DaysGroup'] = counter
+                        elif (operator_df.loc[next_index, 'DaysGroup'] == "") & (
+                                operator_df.loc[current_index, 'DaysGroup'] != ""):
                             operator_df.at[next_index, 'DaysGroup'] = operator_df.loc[current_index, 'DaysGroup']
+                            group.at[next_index, 'DaysGroup'] = group.loc[current_index, 'DaysGroup']
                         else:
                             operator_df.at[current_index, 'DaysGroup'] = operator_df.loc[next_index, 'DaysGroup']
+                            group.at[current_index, 'DaysGroup'] = group.loc[next_index, 'DaysGroup']
         if operator_df.loc[current_index, 'DaysGroup'] == "":
             operator_df.loc[current_index, 'DaysGroup'] = counter
+            group.loc[current_index, 'DaysGroup'] = counter
 
 operator_df.to_csv('C:\\Users\\irai\\OneDrive - KPMG\\BODS\\BDE '
-                  'Package\\src\\BODSDataExtractor\\test_after_days_group_24.02.2023.csv')
+                  'Package\\src\\BODSDataExtractor\\test_after_days_group_02.03.2023_new.csv')
 
-#Create an empty report_df DataFrame to store the results
+# Create an empty report_df DataFrame to store the results
 report_df = pd.DataFrame(
     columns=['LicenseNumber', 'ServiceCode', 'LineName', 'Look_ahead_missing_flag', 'Dates_for_missing_lookahead',
              'multiple_valid_files_issue_flag', 'Dates_for_multiple_valid_files', 'OperatingDays'])
 
+# Group operator_df by ServiceCode, LineName and DaysGroup to determine which file is valid for each date.
 groups = operator_df.groupby(['ServiceCode', 'LineName', 'DaysGroup'])
 
 # Iterate over the groups, iterate through each date column and determine which file(s) are valid. If not valid, assign
@@ -183,14 +200,14 @@ for name, group in groups:
          'OperatingDays': [group['OperatingDays'].iloc[-1]]})
     report_df = pd.concat([report_df, temp_df], ignore_index=True)
 
-#Edit below paths for outputs
+# Edit below paths for outputs
 operator_df.to_csv(
-   'C:\\Users\\irai\\OneDrive - KPMG\\BODS\\BDE Package\\src\\BODSDataExtractor\\operator_df_24.02.2023.csv')
-consumer_df = operator_df.dropna(subset=dates, how='all')  # do we want to drop n/a values for the consumer??
+    'C:\\Users\\irai\\OneDrive - KPMG\\BODS\\BDE Package\\src\\BODSDataExtractor\\operator_df_02.03.2023_new.csv')
+consumer_df = operator_df.dropna(subset=dates, how='all')
 consumer_df.to_csv(
-   'C:\\Users\\irai\\OneDrive - KPMG\\BODS\\BDE Package\\src\\BODSDataExtractor\\consumer_df_24.02.2023.csv')
+    'C:\\Users\\irai\\OneDrive - KPMG\\BODS\\BDE Package\\src\\BODSDataExtractor\\consumer_df_02.03.2023_new.csv')
 report_df.to_csv(
-   'C:\\Users\\irai\\OneDrive - KPMG\\BODS\\BDE Package\\src\\BODSDataExtractor\\report_df_24.02.2023.csv')
+    'C:\\Users\\irai\\OneDrive - KPMG\\BODS\\BDE Package\\src\\BODSDataExtractor\\report_df_02.03.2023_new.csv')
 
 # consumer to query
 # def getValidFile(date, serviceCode, lineName, operatingDays): date = pd.to_datetime(date) try:

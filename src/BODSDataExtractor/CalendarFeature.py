d11ba5a6eeda7a74184592a57c748403a9c77944
Isri Rai <isri.rai@kpmg.co.uk>
2023-03-16 12:13:19 +0000

    Edited extractor file (services_on_bods_or_otc_by_area_just_otc & services_on_bods_or_otc_by_area)

--- src/BODSDataExtractor/CalendarFeature.py
+++ src/BODSDataExtractor/CalendarFeature.py
@@ -6,7 +6,7 @@ Created on Wed Jan  4 12:37:43 2023
 """
 
 # Versioning
-# version: 1.2
+# version: 1.3
 
 from extractor import TimetableExtractor
 import pandas as pd
@@ -26,10 +26,10 @@ my_bus_data_object = TimetableExtractor(api_key=api
 
 # Returns a copy of the service line level data suitable for analysis and filters columns
 analytical_timetable_data_without_duplicates = my_bus_data_object.analytical_timetable_data_analysis()
-calendar_df = analytical_timetable_data_without_duplicates[['DatasetID', 'OperatorName', 'FileName', 'TradingName',
-                                                            'ServiceCode', 'LineName', 'OperatingPeriodStartDate',
-                                                            'OperatingPeriodEndDate', 'RevisionNumber',
-                                                            'OperatingDays']]
+timetable_df = analytical_timetable_data_without_duplicates[['DatasetID', 'OperatorName', 'FileName', 'TradingName',
+                                                             'ServiceCode', 'LineName', 'OperatingPeriodStartDate',
+                                                             'OperatingPeriodEndDate', 'RevisionNumber',
+                                                             'OperatingDays']]
 
 
 # for testing
@@ -38,49 +38,47 @@ calendar_df = analytical_timetable_data_without_duplicates[['DatasetID', 'Operat
 
 def produce_calendar_structure(dataframe):
     """
-    This function adds columns to the end of the dataframe. Each column is assigned a date going from today's date to
-    42 days from now. It then assigns true for the operating period.
+    This function adds columns to the end of the dataframe. Each column is assigned a date starting from today's date
+    up to 42 days from now. It then assigns the value true for the dates within the operating period.
 
     Parameters:
     dataframe (df): dataframe of timetable data
 
     Returns:
-    dataframe: dataframe with calendar dates added with true values to show the operating period.
+    calendar_df (df): dataframe with calendar dates added with true values indicating the operating period dates.
     """
     current_date = pd.to_datetime('today').normalize()
     expected_final_published_date = current_date + pd.Timedelta(days=42)
 
-    # Add the dates to the calendar_df and assign all values as empty
+    calendar_df = dataframe
     delta = dt.timedelta(days=1)
     while current_date <= expected_final_published_date:
-        dataframe[current_date] = None
+        calendar_df[current_date] = None
         print(current_date, end='\n')
         current_date += delta
 
-    # Convert data types for analysis
-    dataframe['OperatingPeriodStartDate'] = pd.to_datetime(dataframe['OperatingPeriodStartDate'])
-    dataframe['OperatingPeriodEndDate'] = pd.to_datetime(dataframe['OperatingPeriodEndDate'])
-    dataframe.reset_index(inplace=True)
+    calendar_df['OperatingPeriodStartDate'] = pd.to_datetime(calendar_df['OperatingPeriodStartDate'])
+    calendar_df['OperatingPeriodEndDate'] = pd.to_datetime(calendar_df['OperatingPeriodEndDate'])
+    calendar_df.reset_index(inplace=True)
 
-    # Iterate through each calendar date column. For each date iterate through each row and assign value 'true'
-    # where calendar date is between or equal to the operating period start and end date
+    # update this to make it generic!
     for calendar_date in range(11, 54):
-        for row in dataframe.itertuples():
-            print(dataframe.columns[calendar_date])
+        for row in calendar_df.itertuples():
+            print(calendar_df.columns[calendar_date])
             print(getattr(row, 'OperatingPeriodStartDate'))
             if (getattr(row, 'OperatingPeriodEndDate') is pd.NaT) or (getattr(row, 'OperatingPeriodEndDate') is None):
-                print(dataframe.columns[calendar_date])
+                print(calendar_df.columns[calendar_date])
                 print(getattr(row, 'OperatingPeriodStartDate'))
-                if dataframe.columns[calendar_date] >= getattr(row, 'OperatingPeriodStartDate'):
-                    dataframe[dataframe.columns[calendar_date]][getattr(row, 'Index')] = "True"
-            elif (dataframe.columns[calendar_date] >= getattr(row, 'OperatingPeriodStartDate')) and \
-                    (dataframe.columns[calendar_date] <= getattr(row, 'OperatingPeriodEndDate')):
-                dataframe[dataframe.columns[calendar_date]][getattr(row, 'Index')] = "True"
-    return dataframe
+                if calendar_df.columns[calendar_date] >= getattr(row, 'OperatingPeriodStartDate'):
+                    calendar_df[calendar_df.columns[calendar_date]][getattr(row, 'Index')] = "True"
+            elif (calendar_df.columns[calendar_date] >= getattr(row, 'OperatingPeriodStartDate')) and \
+                    (calendar_df.columns[calendar_date] <= getattr(row, 'OperatingPeriodEndDate')):
+                calendar_df[calendar_df.columns[calendar_date]][getattr(row, 'Index')] = "True"
+    return calendar_df
 
 
 # Adding Calendar dates
-calendar_df_structure = produce_calendar_structure(calendar_df)
+calendar_df_structure = produce_calendar_structure(timetable_df)
 
 
 # for testing
@@ -89,22 +87,23 @@ calendar_df_structure = produce_calendar_structure(calendar_df)
 
 def convert_dates(dataframe):
     """
-    This function substitutes the operating period start date, where it has been summarised to include all dates
+    This function substitutes the operating period start date to include all dates
     E.g. Monday-Friday -> Monday, Tuesday, Wednesday, Thursday, Friday
 
     Parameters:
-    dataframe (df): dataframe with operating period column
+        dataframe (df): dataframe with operating period column
 
     Returns:
-    dataframe: dataframe with operating period refactored.
+        refactored_dataframe (df): dataframe with operating period refactored.
     """
     dataframe = dataframe.sort_values(by=['ServiceCode', 'LineName', 'RevisionNumber', 'OperatingDays']
                                       , ascending=True)
 
-    dataframe = dataframe.reset_index()
+    refactored_dataframe = dataframe
+    refactored_dataframe = refactored_dataframe.reset_index()
 
     # Converts dates with dashes e.g. Monday-Friday to Monday, Tuesday, Wednesday, Thursday, Friday to enable comparison
-    for operatingDay in dataframe['OperatingDays']:
+    for operatingDay in refactored_dataframe['OperatingDays']:
         if "-" in operatingDay:
             first_date = operatingDay[0:operatingDay.find('-')]
             second_date = operatingDay[operatingDay.find('-') + 1:]
@@ -113,8 +112,8 @@ def convert_dates(dataframe):
             for i in range(days[first_date], days[second_date] + 1):
                 new_output.append(list(days.keys())[list(days.values()).index(i)])
             new_output = ",".join(new_output)
-            dataframe.replace(operatingDay, new_output, inplace=True)
-    return dataframe
+            refactored_dataframe.replace(operatingDay, new_output, inplace=True)
+    return refactored_dataframe
 
 
 # Converting format of dates
@@ -126,21 +125,30 @@ calendar_df_refactored = convert_dates(calendar_df_structure)
 
 
 def check_operating_days(dataframe):
-    # Create new column to group rows where the operating period for a service-line combo is a subset of an equal/higher
-    # revision number file
-    dataframe['DaysGroup'] = ""
-    dataframe.sort_values(by=['ServiceCode', 'LineName', 'RevisionNumber'])
-    groups = dataframe.groupby(['ServiceCode', 'LineName'])
+    """
+    This function creates a new column to group rows where the operating days for a service-line combo is a subset of
+    an equal or higher revision number file's operating days.
 
-    # Iterate through each row in the group (current_index) and for each row iterate through the next rows (next_index).
-    # Check if all the operating days for the current_index are in the next_index's operating days. If they are a subset
-    # assign both rows the same counter in the 'DaysGroup' column
+    Logic: It iterates through each row in the group (current_index) and for each row it iterates through the next rows
+    (next_index). It checks if all the operating days for the current_index are in the next_index's operating days.
+    If they are a subset, it assigns both rows the same counter in the 'DaysGroup' column. This will form the new group.
+
+    Parameters:
+        dataframe (df): dataframe with service, line and operating days
+
+    Returns:
+        dataframe_with_grouping (df): dataframe with new column called 'DaysGroup' which tells us the groups for
+        checking which file is valid for a specific date.
+    """
+    dataframe_with_grouping = dataframe
+    dataframe_with_grouping['DaysGroup'] = ""
+    dataframe_with_grouping.sort_values(by=['ServiceCode', 'LineName', 'RevisionNumber'])
+    groups = dataframe_with_grouping.groupby(['ServiceCode', 'LineName'])
+
     counter = 0
     for name, group in groups:
         for current_index in group.index:
             counter += 1
-            # check if the revision numbers are all the same. If they are the same then 'next_index' goes through all
-            # the rows. If they are not the same then only go through the rows with a higher revision number.
             if group.RevisionNumber.nunique() == 1:
                 calculated_next_index = group.iloc[0].name
             else:
@@ -154,23 +162,21 @@ def check_operating_days(dataframe):
                         operating_days_in_current_index = operating_days_in_current_index.split(",")
                         operating_days_next_index = operating_days_next_index.split(",")
                         if all([item in operating_days_next_index for item in operating_days_in_current_index]):
-                            if (dataframe.loc[next_index, 'DaysGroup'] == "") & (
-                                    dataframe.loc[current_index, 'DaysGroup'] == ""):
-                                dataframe.at[current_index, 'DaysGroup'] = counter
-                                dataframe.at[next_index, 'DaysGroup'] = counter
-                                group.at[current_index, 'DaysGroup'] = counter
-                                group.at[next_index, 'DaysGroup'] = counter
-                            elif (dataframe.loc[next_index, 'DaysGroup'] == "") & (
-                                    dataframe.loc[current_index, 'DaysGroup'] != ""):
-                                dataframe.at[next_index, 'DaysGroup'] = dataframe.loc[current_index, 'DaysGroup']
-                                group.at[next_index, 'DaysGroup'] = group.loc[current_index, 'DaysGroup']
+                            if (dataframe_with_grouping.loc[next_index, 'DaysGroup'] == "") & (
+                                    dataframe_with_grouping.loc[current_index, 'DaysGroup'] == ""):
+                                dataframe_with_grouping.at[current_index, 'DaysGroup'] = counter
+                                dataframe_with_grouping.at[next_index, 'DaysGroup'] = counter
+                            elif (dataframe_with_grouping.loc[next_index, 'DaysGroup'] == "") & (
+                                    dataframe_with_grouping.loc[current_index, 'DaysGroup'] != ""):
+                                dataframe_with_grouping.at[next_index, 'DaysGroup'] = \
+                                    dataframe_with_grouping.loc[current_index, 'DaysGroup']
                             else:
-                                dataframe.at[current_index, 'DaysGroup'] = dataframe.loc[next_index, 'DaysGroup']
+                                dataframe_with_grouping.at[current_index, 'DaysGroup'] = \
+                                    dataframe_with_grouping.loc[next_index, 'DaysGroup']
                                 group.at[current_index, 'DaysGroup'] = group.loc[next_index, 'DaysGroup']
-            if dataframe.loc[current_index, 'DaysGroup'] == "":
-                dataframe.loc[current_index, 'DaysGroup'] = counter
-                group.loc[current_index, 'DaysGroup'] = counter
-    return dataframe
+            if dataframe_with_grouping.loc[current_index, 'DaysGroup'] == "":
+                dataframe_with_grouping.loc[current_index, 'DaysGroup'] = counter
+    return dataframe_with_grouping
 
 
 # Determine groups for validity check
@@ -180,21 +186,33 @@ calendar_df_grouped = check_operating_days(calendar_df_refactored)
 
 # Gets a list of all dates from calendar_df
 columns = []
-for date in calendar_df.columns:
+for date in timetable_df.columns:
     columns.append(date)
 dates = columns[11:]
 
 
 def determine_file_validity(dataframe):
-    # Create an empty report_df DataFrame to store the results
+    """
+    This function groups by the ServiceCode, LineName and DaysGroups and iterates through each date column to check
+    if the files are valid. If they are valid the row will be given a value of True. If they are not valid they will
+    be given no value (empty).
+
+    Parameters:
+        dataframe (df): dataframe with calendar dates (including service code, line name and days group)
+        It relies on the 'check_operating_days' function.
+
+    Returns:
+        operator_df (df): dataframe with values in calendar_df updated with empty values where a file is not valid
+        for a specific date
+        report_df (df): summarised view which identifies for each service-line-operating days
+        combination, if there is a missing 42-day lookahead or if there are multiple valid files on a specific date.
+    """
     report_df = pd.DataFrame(
         columns=['LicenseNumber', 'ServiceCode', 'LineName', 'Look_ahead_missing_flag', 'Dates_for_missing_lookahead',
                  'multiple_valid_files_issue_flag', 'Dates_for_multiple_valid_files', 'OperatingDays', 'DatasetID'])
 
-    # Group operator_df by ServiceCode, LineName and DaysGroup to determine which file is valid for each date.
-    groups = dataframe.groupby(['ServiceCode', 'LineName', 'DaysGroup'])
-    # Iterate over the groups, iterate through each date column and determine which file(s) are valid. If not valid,
-    # assign column date value to null
+    operator_df = dataframe
+    groups = operator_df.groupby(['ServiceCode', 'LineName', 'DaysGroup'])
     for name, group in groups:
         # Find the index of the max revision number
         max_revision = group['RevisionNumber'].astype('int').idxmax()
@@ -220,10 +238,10 @@ def determine_file_validity(dataframe):
                     # if the index of the row is less than the index of the row with the greater revision number
                     # for only 'True' rows, then set this value to empty
                     if index < true_rows_max_revision_number_index:
-                        dataframe.loc[index, col] = ''
+                        operator_df.loc[index, col] = ''
                     # if the revision number of the row is equal to the greatest revision number of the 'True' rows
                     # then there are multiple valid files for the specified date
-                    if (dataframe.loc[index, 'RevisionNumber'] == str(true_rows_max_revision_number)) & \
+                    if (operator_df.loc[index, 'RevisionNumber'] == str(true_rows_max_revision_number)) & \
                             (len(true_rows[true_rows['RevisionNumber'] == str(true_rows_max_revision_number)]) > 1):
                         multiple_valid_files_issue_flag = True
                         dates_for_multiple_valid_files.append(col)
@@ -246,19 +264,33 @@ def determine_file_validity(dataframe):
              'OperatingDays': [group['OperatingDays'].iloc[-1]],
              'DatasetID': [datasets]})
         report_df = pd.concat([report_df, temp_df], ignore_index=True)
-    return dataframe, report_df
+    return operator_df, report_df
 
 
 # Determine which file is valid on a specific date
-operator_df, calendar_report_df = determine_file_validity(calendar_df_grouped)
-operator_df.to_csv('operator_{}.csv'.format(pd.to_datetime('today').strftime("%Y-%m-%d %Hh%Mm%Ss")))
-consumer_df = operator_df.dropna(subset=dates, how='all')
+calendar_operator_df, calendar_report_df = determine_file_validity(calendar_df_grouped)
+calendar_operator_df.to_csv('operator_{}.csv'.format(pd.to_datetime('today').strftime("%Y-%m-%d %Hh%Mm%Ss")))
+consumer_df = calendar_operator_df.dropna(subset=dates, how='all')
 consumer_df.to_csv('consumer_{}.csv'.format(pd.to_datetime('today').strftime("%Y-%m-%d %Hh%Mm%Ss")))
 calendar_report_df.to_csv('calendar_report_{}.csv'.format(pd.to_datetime('today').strftime("%Y-%m-%d %Hh%Mm%Ss")))
 
 
 # consumer to query
 def get_valid_file(date_for_file, service_code, line_name, operating_days):
+    """
+    This function allows a consumer to query a date, service code, line name and operating days and receive the
+    corresponding valid file if there is one. Otherwise, it will return 'No valid file' or an error if there is a
+    problem with the input.
+
+    Parameters:
+        date_for_file: the date you want to find the valid file for
+        service_code: Service Code
+        line_name: Line Name
+        operating_days: Operating Days
+
+    Returns:
+        response: corresponding valid file(s) or 'No valid file' or error if input is incorrect.
+    """
     date_for_file = pd.to_datetime(date_for_file)
     try:
         rows_to_check = consumer_df.loc[((consumer_df['ServiceCode'] == service_code) &
@@ -277,6 +309,7 @@ def get_valid_file(date_for_file, service_code, line_name, operating_days):
         raise IndexError("No record found for provided input.")
 
 
-result = get_valid_file('13/03/2023  00:00:00', 'PB0000815:112', '541',
+# update below inputs
+result = get_valid_file('16/03/2023  00:00:00', 'PB0000815:112', '541',
                         'Tuesday,Wednesday,Thursday,Friday,Saturday')
 print(result)
